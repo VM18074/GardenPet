4a9a02723fa0de765f5792aaa0fbf0d8ecb25c3a
VM18074 <vm18074@ues.edu.sv>
2025-02-23 02:01:10 -0600

    update
    create prject with angular

---
+++ GardenPet/src/app/pages/moreinfo/moreinfo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moreinfo',
+  imports: [],
+  templateUrl: './moreinfo.component.html',
+  styleUrl: './moreinfo.component.css'
+})
+export class MoreinfoComponent {
+
+}
