3f15474eb938ced69599ee59b99c8211e453dc6f
VM18074 <vm18074@ues.edu.sv>
2025-02-23 02:25:40 -0600

    update
    Funciona menu y redireccion a paginas falta añadir estilos

--- GardenPet/src/app/app.routes.ts
+++ GardenPet/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DonateComponent } from './pages/donate/donate.component';
+import { VolunteerComponent } from './pages/volunteer/volunteer.component';
+import { AdoptComponent } from './pages/adopt/adopt.component';
+import { MoreinfoComponent } from './pages/moreinfo/moreinfo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'donate', component: DonateComponent },
+  { path: 'volunteer', component: VolunteerComponent },
+  { path: 'adopt', component: AdoptComponent },
+  { path: 'moreinfo', component: MoreinfoComponent },
+  { path: '**', redirectTo: '/home' }
+];
