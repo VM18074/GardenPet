8825f2e96e96b346578fd035b48881d22aff17c9
VM18074 <vm18074@ues.edu.sv>
2025-03-14 00:40:06 -0600

    update
    Todos los diseños realizados y optimizados para moviles solo falta la pagina de moreinfo

--- GardenPet/src/app/pages/donate/donate.component.ts
+++ GardenPet/src/app/pages/donate/donate.component.ts
@@ -9,4 +9,13 @@ import { RouterModule } from '@angular/router';
 })
 export class DonateComponent {
 
+  copyText(text: string) {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log('Texto copiado al portapapeles:', text);
+      // Aquí puedes mostrar una notificación al usuario, por ejemplo, usando un Toast o similar.
+    }).catch(err => {
+      console.error('Error al copiar texto:', err);
+    });
+  }
+
 }
--- GardenPet/src/app/pages/volunteer/volunteer.component.ts
+++ GardenPet/src/app/pages/volunteer/volunteer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-volunteer',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './volunteer.component.html',
   styleUrl: './volunteer.component.css'
 })
